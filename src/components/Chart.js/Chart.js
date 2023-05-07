622cab84c25f5118757a10a4830a7399ad89dbdf
{정경희} <kyeonghee_@naver.com>
2023-05-08 06:33:41 +0900

    코드 간결하게 정리

--- src/components/Chart.js/Chart.js
+++ src/components/Chart.js/Chart.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ResponsiveContainer, PieChart, Pie, Legend } from 'recharts';
 
+/*
+조건 1: 상위 5개의 리콜 사유 데이터만 추출
+조건 2: 불러온 데이터들을 1~5순위에 맞춰 차례대로 차트에 반영
+조건 3: 리콜 사유 데이터들의 합은 무조건 100%
+조건 4: 소수점 첫째자리까지만 반영. 두 번째자리 이상으로 나올 경우 반올림
+*/
+
 export default function Chart() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('url/recall-reasons') // 리콜 사유 data
+    axios.get('url/recall-reasons') // 리콜 사유 data get
       .then(response => {
         const sortedData = response.data.sort((a, b) => b.value - a.value).slice(0, 5); // 상위 5개 데이터만 불러오기
         const updatedData = calculatePercentages(sortedData);
@@ -17,19 +24,8 @@ export default function Chart() {
       });
   }, []);
 
-  // 각 항목의 백분율 계산 함수
-//   const calculatePercentages = (dataArray) => {
-//     const total = dataArray.reduce((accumulator, currentValue) => {
-//       return accumulator + currentValue.value;
-//     }, 0);
-//     return dataArray.map(item => {
-//       return {
-//         ...item,
-//         percent: parseFloat(((item.value / total) * 100).toFixed(1)) // 소숫점 첫째자리까지만 표시
-//       };
-//     });
-//   };
-const calculatePercentages = (dataArray) => { // 함수 수정
+// 각 항목의 백분율 계산 함수
+const calculatePercentages = (dataArray) => {
     const total = dataArray.reduce((accumulator, currentValue) => {
       return accumulator + currentValue.value;
     }, 0);
@@ -48,19 +44,8 @@ const calculatePercentages = (dataArray) => { // 함수 수정
   };
   
 
-  // 업데이트된 데이터를 다시 계산해서 state에 저장
-//   const updateData = () => {
-//     axios.get('url/recall-reasons')
-//       .then(response => {
-//         const sortedData = response.data.sort((a, b) => b.value - a.value).slice(0, 5); // 상위 5개 데이터만 불러오기
-//         const updatedData = calculatePercentages(sortedData);
-//         setData(updatedData);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   };
-const updateData = () => { // 함수 수정
+// 업데이트된 데이터를 다시 계산해서 state에 저장
+const updateData = () => {
     axios.get('url/recall-reasons')
       .then(response => {
         const updatedData = calculatePercentages(response.data);
@@ -85,17 +70,9 @@ const updateData = () => { // 함수 수정
     <div style={{ width: '100%', height: 300 }}>
       <ResponsiveContainer>
         <PieChart>
-          {/* <Pie dataKey="value" data={data} fill="#8884d8" label /> */}
           <Pie dataKey="value" data={data} nameKey="name" legendType="circle" fill="#8884d8" label={(props) => `${props.name} ${props.percent}%`} />
           <Legend />
         </PieChart>
-        {/* <div> */}
-          {/* <ul> */}
-            {/* {sortData(data).map((item, index) => ( */}
-              {/* <li key={index}>{`${index+1}. ${item}`}</li> */}
-            {/* ))} */}
-          {/* </ul> */}
-        {/* </div> */}
       </ResponsiveContainer>
     </div>
   );
--- src/pages/RecallPage.js
+++ src/pages/RecallPage.js
@@ -7,33 +7,5 @@ const ReCallPage = () => {
         <Chart/>
     )
 }
-// const recallView = styled.div`
-    // width: 100%;
-// `
-
-// const recallMenu = styled.div`
-// ul.recall {
-//         list-style: none;
-//         display: flex;
-//         flex-direction: row;
-//     }
-// .recallText {
-
-// }
-// `
-
-// const ReCallPage = () => {
-//     return (
-//         <div>
-//             <HeaderContainer/>
-//                 <recallMenu>
-//                     <ul className="recall">
-//                         <li className="recallText">하위 Top 3</li>
-//                         <li className="recallText">상위 Top 3</li>
-//                     </ul>
-//                 </recallMenu>
-//         </div>
-//     )
-// }
 
 export default ReCallPage;
