c494027af30ea6fd680a59ffced1cc19196e8f90
{정경희} <kyeonghee_@naver.com>
2023-05-13 22:58:23 +0900

    reCallPage Chart

--- src/components/Chart.js/Chart.js
+++ src/components/Chart.js/Chart.js
@@ -1,79 +1,112 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { ResponsiveContainer, PieChart, Pie, Legend } from 'recharts';
+import { PieChart, Pie, Legend, Cell, ResponsiveContainer } from 'recharts';
 
-/*
-조건 1: 상위 5개의 리콜 사유 데이터만 추출
-조건 2: 불러온 데이터들을 1~5순위에 맞춰 차례대로 차트에 반영
-조건 3: 리콜 사유 데이터들의 합은 무조건 100%
-조건 4: 소수점 첫째자리까지만 반영. 두 번째자리 이상으로 나올 경우 반올림
-*/
-
-export default function Chart() {
-  const [data, setData] = useState([]);
+const Chart = () => {
+  const [datas, setDatas] = useState([]);
 
   useEffect(() => {
-    axios.get('url/recall-reasons') // 리콜 사유 data get
-      .then(response => {
-        const sortedData = response.data.sort((a, b) => b.value - a.value).slice(0, 5); // 상위 5개 데이터만 불러오기
-        const updatedData = calculatePercentages(sortedData);
-        setData(updatedData);
+    axios
+      .get('http://13.125.169.58:5000/recall/data')
+      .then((response) => {
+        var array = [];
+
+        response.data.sort(function (a, b) {
+          array.push(a.reasons);
+        });
+
+        const result = array.reduce((accu, curr) => {
+          accu[curr] = (accu[curr] || 0) + 1;
+          return accu;
+        }, {});
+
+        const sortedData2 = Object.entries(result)
+          .sort(function (a, b) {
+            return b[1] - a[1];
+          })
+          .slice(0, 5);
+
+        let obj = {};
+        let list = [];
+
+        for (var i = 0; i < sortedData2.length; i++) {
+          obj = { name: sortedData2[i][0], value: sortedData2[i][1] };
+          list.push(obj);
+        }
+
+        setDatas(list);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
 
-// 각 항목의 백분율 계산 함수
-const calculatePercentages = (dataArray) => {
-    const total = dataArray.reduce((accumulator, currentValue) => {
-      return accumulator + currentValue.value;
-    }, 0);
-  
-    const sortedData = dataArray.sort((a, b) => b.value - a.value); // value를 기준으로 내림차순 정렬
-    const slicedData = sortedData.slice(0, 5); // 상위 5개 데이터만 추출
-  
-    const updatedData = slicedData.map(item => {
-      return {
-        ...item,
-        percent: parseFloat((item.value / total) * 100).toFixed(1) // 백분율을 계산하고 소숫점 첫째자리까지 반올림
-      };
-    });
-  
-    return updatedData;
-  };
-  
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-// 업데이트된 데이터를 다시 계산해서 state에 저장
-const updateData = () => {
-    axios.get('url/recall-reasons')
-      .then(response => {
-        const updatedData = calculatePercentages(response.data);
-        setData(updatedData);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-  useEffect(() => {
-    const intervalId = setInterval(updateData, 5000); // 5초마다 업데이트
-    return () => clearInterval(intervalId);
-  }, []);
-
-  // 데이터 순서대로 정렬하는 함수
-  const sortData = (dataArray) => {
-    return dataArray.sort((a, b) => b.value - a.value).map(item => item.name);
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
   };
 
   return (
-    <div style={{ width: '100%', height: 300 }}>
-      <ResponsiveContainer>
-        <PieChart>
-          <Pie dataKey="value" data={data} nameKey="name" legendType="circle" fill="#8884d8" label={(props) => `${props.name} ${props.percent}%`} />
-          <Legend />
-        </PieChart>
-      </ResponsiveContainer>
-    </div>
+    <>
+      <div>
+        <div class="row d-flex justify-content-center text-center">
+          <hr />
+          <div className="col-md-8">
+            <ResponsiveContainer
+              width={900}
+              height={900}
+              className="text-center"
+            >
+              <PieChart width={400} height={400}>
+                <Legend layout="vertical" verticalAlign="top" align="top" />
+                <Pie
+                  data={datas}
+                  cx="50%"
+                  cy="5000%"
+                  labelLine={false}
+                  label={renderCustomizedLabel}
+                  outerRadius={180}
+                  fill="#8884d8"
+                  dataKey="value"
+                >
+                  {datas.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Chart;
