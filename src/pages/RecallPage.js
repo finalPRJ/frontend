1ef20066dd4ce4d8b68a29b4059597ecf6f5016c
{정경희} <kyeonghee_@naver.com>
2023-05-20 03:45:08 +0900

    recallPage chart, wordClude 수정

--- src/pages/RecallPage.js
+++ src/pages/RecallPage.js
@@ -1,12 +1,16 @@
 import HeaderContainer from "../containers/common/HeaderContainer";
-import Chart from "../components/Chart.js/Chart";
+import Chart from "../components/recall/Chart";
+import WordClude from "../components/recall/WordClude";
+import RecallList from "../components/recall/RecallList";
 
 const ReCallPage = () => {
     return (
-        <HeaderContainer>
+        <div>
+            <HeaderContainer/>
+            <RecallList />
             <Chart/>
-        </HeaderContainer>
-        
+            <WordClude/>
+        </div>
     )
 }
 
